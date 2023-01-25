849ba8a93b25e05ef0acaeec8b86d713612d0de0
vonSch4 <115642881+vonSch4@users.noreply.github.com>
2023-01-25 19:21:43 +0500

    refactor: refactoring TaskList

--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -5,26 +5,24 @@ import Task from '../Task';
 export default function TaskList(props) {
   const { todoData, deleteItem, editItem, onToggleDone } = props;
 
+  const listItem = todoData.map((prop) => {
+    const { id: key } = prop;
+    return (
+      <Task
+        key={key}
+        {...prop}
+        deleteItem={deleteItem}
+        editItem={editItem}
+        onToggleDone={onToggleDone}
+      />
+    );
+  });
+
   if (todoData.length === 0) {
     return <div className="empty-list">The task list is empty</div>;
   }
 
-  return (
-    <ul className="todo-list">
-      {todoData.map((prop) => {
-        const { id: key } = prop;
-        return (
-          <Task
-            key={key}
-            {...prop}
-            deleteItem={deleteItem}
-            editItem={editItem}
-            onToggleDone={onToggleDone}
-          />
-        );
-      })}
-    </ul>
-  );
+  return <ul className="todo-list">{listItem}</ul>;
 }
 
 TaskList.defaultProps = {
